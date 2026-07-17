4c31a58fbc994288f85e5536e260897cff281e09
CPT-Rabbit <V.ragozin@gmx.de>
2026-07-17 21:52:05 +0200

    Sanitized snapshot: docs: record public repository description

--- konsilium/egress.py
+++ konsilium/egress.py
@@ -9,7 +9,7 @@ class EgressViolation(ValueError):
 
 _PII_TOKEN = re.compile(r"\[(?:PATIENT|DOB|ADDR|INSURANCE|EMAIL|PHONE)_\d+\]")
 _PHONE = re.compile(
-    r"\b(?!\d{4}-\d{2}-\d{2}\b)(?!\d{1,2}[./-]\d{1,2}[./-]\d{2,4}\b)"
+    r"\b(?!\d{4}-\d{2}-\d{2}\b)(?!\d{1,2}[./-]\d{1,2}[./-]\d{2,4}\b)(?!\d{4}[/-]\d{4}\b)"
     r"\+?[0-9][0-9 ()/-]{7,}[0-9]\b"
 )
 
